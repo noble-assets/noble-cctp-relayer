0bd414deeb5c20f607d6f125c8ff67cc4e2cf742
Joel Smith <joelsmith.2019@gmail.com>
2024-05-06 16:32:24 -0500

    Fix URL Parsing & Redundant Timeout

--- circle/attestation.go
+++ circle/attestation.go
@@ -15,13 +15,6 @@ import (
 
 // CheckAttestation checks the iris api for attestation status and returns true if attestation is complete
 func CheckAttestation(attestationURL string, logger log.Logger, irisLookupID string, txHash string, sourceDomain, destDomain types.Domain) *types.AttestationResponse {
-	logger.Debug(fmt.Sprintf("Checking attestation for %s%s%s for source tx %s from %d to %d", attestationURL, "0x", irisLookupID, txHash, sourceDomain, destDomain))
-
-	client := http.Client{Timeout: 2 * time.Second}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
 	// append ending / if not present
 	if attestationURL[len(attestationURL)-1:] != "/" {
 		attestationURL += "/"
@@ -32,22 +25,30 @@ func CheckAttestation(attestationURL string, logger log.Logger, irisLookupID str
 		irisLookupID = "0x" + irisLookupID
 	}
 
+	logger.Debug(fmt.Sprintf("Checking attestation for %s%s for source tx %s from %d to %d", attestationURL, irisLookupID, txHash, sourceDomain, destDomain))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, attestationURL+irisLookupID, nil)
 	if err != nil {
 		logger.Debug("error creating request: " + err.Error())
 		return nil
 	}
 
+	client := http.Client{}
 	rawResponse, err := client.Do(req)
 	if err != nil {
 		logger.Debug("error during request: " + err.Error())
 		return nil
 	}
+
 	defer rawResponse.Body.Close()
 	if rawResponse.StatusCode != http.StatusOK {
 		logger.Debug("non 200 response received from Circles attestation API")
 		return nil
 	}
+
 	body, err := io.ReadAll(rawResponse.Body)
 	if err != nil {
 		logger.Debug("unable to parse message body")
@@ -60,7 +61,8 @@ func CheckAttestation(attestationURL string, logger log.Logger, irisLookupID str
 		logger.Debug("unable to unmarshal response")
 		return nil
 	}
-	logger.Info(fmt.Sprintf("Attestation found for %s%s%s", attestationURL, "0x", irisLookupID))
+
+	logger.Info(fmt.Sprintf("Attestation found for %s%s", attestationURL, irisLookupID))
 
 	return &response
 }
